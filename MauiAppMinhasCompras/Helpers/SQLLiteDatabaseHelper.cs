5f6ebf2e20f70ae235ac58387642f841c9c6d661
Usuario <Usuario@CPD03-M10>
2024-11-04 15:10:28 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Helpers/SQLLiteDatabaseHelper.cs
@@ -0,0 +1,24 @@
+﻿using MauiAppMinhasCompras.Models;
+using SQLite;
+
+namespace MauiAppMinhasCompras.Helpers
+{
+    public class SQLiteDatabaseHelper
+    {
+
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+
+        public Task<List<Produto>> Update(Produto p)
+        {
+
+            string sql = "UPDATE P";
+
+        }
+    }
+}
---
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MauiAppMinhasCompras.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NovoProduto : ContentView
+    {
+        public NovoProduto()
+        {
+            InitializeComponent();
+        }
+    }
+}
